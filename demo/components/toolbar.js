1138f661210396c8e999a06061cdf6cdb3b6d59a
Guillaume Bentaieb <guillaume.bentaieb@canal-plus.com>
2018-02-03 10:20:46 +0100

    fix(ui): modify load btn label

--- demo/components/toolbar.js
+++ demo/components/toolbar.js
@@ -43,7 +43,7 @@ class Header extends Component {
               {menuItems}
             </DropDownMenu>
             <ToolbarSeparator />
-            <RaisedButton label='Load Video' primary={true} />
+            <RaisedButton label='Load' primary={true} />
           </ToolbarGroup>
         </Toolbar>
         <br />
